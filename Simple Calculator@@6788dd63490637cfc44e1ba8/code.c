68bb5268c26de69c20e11b076d196250206b6d17
lakshkalra07 <lakshkalra9@gmail.com>
2025-02-07 19:06:46 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,52 +1,26 @@
 #include <stdio.h>
 
-// Function declarations
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
-int divide(int a, int b);
-
 int main() {
     int num1, num2;
     char operator;
+    scanf("%d %d", &num1, &num2);
+    scanf(" %c", &operator);
 
-    // Input the numbers and operator
-    printf("Enter two integers followed by an operator (+, -, *, /): ");
-    scanf("%d %d %c", &num1, &num2, &operator);
-
-    // Perform calculation based on operator using if-else statements
     if (operator == '+') {
-        printf("%d + %d = %d\n", num1, num2, add(num1, num2));
+        printf("%d\n", num1 + num2);
     } else if (operator == '-') {
-        printf("%d - %d = %d\n", num1, num2, subtract(num1, num2));
+        printf("%d\n", num1 - num2);
     } else if (operator == '*') {
-        printf("%d * %d = %d\n", num1, num2, multiply(num1, num2));
+        printf("%d\n", num1 * num2);
     } else if (operator == '/') {
         if (num2 != 0) {
-            printf("%d / %d = %d\n", num1, num2, divide(num1, num2));
+            printf("%d\n", num1 / num2);
         } else {
-            printf("Error: Division by zero!\n");
+            printf("Error: Division by zero.\n");
         }
     } else {
-        printf("Error: Unsupported operator!\n");
+        printf("Error: Invalid operator.\n");
     }
 
     return 0;
-}
-
-// Function definitions
-int add(int a, int b) {
-    return a + b;
-}
-
-int subtract(int a, int b) {
-    return a - b;
-}
-
-int multiply(int a, int b) {
-    return a * b;
-}
-
-int divide(int a, int b) {
-    return a / b;
 }
